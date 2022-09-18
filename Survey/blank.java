74d1a741f730abb4751c60ebc5016995cce14f17
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2022-09-18 15:24:30 +0100

    Add files via upload

---
+++ Survey/blank.java
@@ -0,0 +1,8 @@
+public class blank {
+    
+    for (Map.Entry<String, Account> accountEntry :
+map.entrySet()) {
+print(“UserId - ” + accountEntry.getKey() + “, ” +
+“Account - ” + accountEntry.getValue());
+}
+}
